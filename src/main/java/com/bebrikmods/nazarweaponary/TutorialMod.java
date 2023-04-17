73ef3fcf499eaf795d20ca8f1a8d4bc6581453aa
python-is-a-snake <slap.my.penguin@gmail.com>
2023-04-17 21:38:23 +0300

    - initial commit

---
+++ src/main/java/com/bebrikmods/nazarweaponary/TutorialMod.java
@@ -0,0 +1,10 @@
+package com.bebrikmods.nazarweaponary;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TutorialMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
